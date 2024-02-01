90e923ba36e20360fcd2a9b39d8e094c910a18f0
raife.armstrong <armstrong.raife@etoncollege.org.uk>
2024-02-01 21:05:29 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Raife Armstrong on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
