6e11eb81dbc0a9505b2c88e7b4e119aad8882a8e
raife.armstrong <armstrong.raife@etoncollege.org.uk>
2024-02-05 19:10:01 +0000

    adding modle for divisions and students

---
+++ AbsenceRecorder/moddle/Division.swift
@@ -0,0 +1,18 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Raife Armstrong on 5/2/24.
+//
+
+import Foundation
+
+class Division{
+    let code:String
+    var students:[Student]=[]
+    
+    init(code:String){
+        self.code=code
+    }
+    
+}
---
+++ AbsenceRecorder/moddle/Students.swift
@@ -0,0 +1,21 @@
+//
+//  Students.swift
+//  AbsenceRecorder
+//
+//  Created by Raife Armstrong on 5/2/24.
+//
+
+import Foundation
+
+class Student{
+    let firstname:String
+    let surname:String
+    let birthDay:Date
+    
+    init(firstname:String, surname:String, birthDay:Date){
+        self.firstname=firstname
+        self.surname=surname
+        self.birthDay=birthDay
+    }
+    
+}
